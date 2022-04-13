f787d7559d9a2bdbdd3cbe2c2489c744e390fd5c
nataliadiotto <diottonatalia@gmail.com>
2022-04-13 20:11:46 -0300

    Best Solution

--- src/model/exceptions/DomainException.java
+++ src/model/exceptions/DomainException.java
@@ -1,6 +1,6 @@
 package model.exceptions;
 
-public class DomainException extends Exception {
+public class DomainException extends RuntimeException {
 
 	public DomainException (String msg) { //permitir que eu possa instanciar a minha classe passando uma msg pra ela
 		super(msg);
